a42ef5aefdde95e65fbf41ae739559e0cbf49e43
Hanqing1996 <zhq199622@163.com>
2020-08-02 16:24:20 +0800

    添加 usePosts(我实在不觉得有什么出奇之处)

--- blog/pages/posts/index.tsx
+++ blog/pages/posts/index.tsx
@@ -1,30 +1,10 @@
 import {NextPage} from "next";
 import React, {useEffect, useState} from "react";
-import axios from 'axios'
+import usePosts from "../../hooks/usePosts";
 
 const PostsIndex:NextPage=()=>{
 
-    const [posts,setPosts]=useState([])
-    const [isLoading,setLoading]=useState(false)
-    const [isEmpty,setIsEmpty]=useState(false)
-
-    useEffect(()=>{
-        setLoading(true)
-        axios.get('/api/posts').then(res=>{
-
-            setTimeout(()=>{
-                setPosts(res.data)
-
-                if(res.data.length==0){
-                    setIsEmpty(true)
-                }
-
-                setLoading(false)
-
-
-            },3000)
-        })
-    },[])
+    const {posts,isLoading,isEmpty}=usePosts()
 
     return(
         <div>
